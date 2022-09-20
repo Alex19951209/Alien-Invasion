7aaa7aaa021935f7562e1f7fe76e58ad3ea0bb81
Alex19951209 <ovharuk19951209@gmail.com>
2022-09-20 21:16:23 +0200

    Update the position of all aliens in the fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
 			self._check_events()
 			self.ship.update()
 			self._update_bullets()
+			self._update_aliens()
 			self._update_screen()
 
 
@@ -82,6 +83,11 @@ class AlienInvasion:
 		for bullet in self.bullets.copy():
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
+
+
+	def _update_aliens(self):
+		"""Update the position of all aliens in the fleet."""
+		self.aliens.update()
 
 
 	def _create_fleet(self):

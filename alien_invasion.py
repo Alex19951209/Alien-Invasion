b8035b689a8c43e4040094be79a1e674093dd901
Alex19951209 <ovharuk19951209@gmail.com>
2022-09-20 19:12:48 +0200

    Creating a class with settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
 	"""A generic class that controls game resources and behavior."""
 
 	def __init__(self):
 		"""Initialize game, create game resources."""
 		pygame.init()
+		self.settings = Settings()
 
-		self.screen = pygame.display.set_mode((1200, 650))
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
 
 
@@ -22,8 +26,11 @@ class AlienInvasion:
 				if event.type == pygame.QUIT:
 					sys.exit()
 
-		# Show the last normal screen.
-		pygame.display.flip()
+				# Redraw the screen on each loop interpretation.
+				self.screen.fill(self.settings.bg_color)
+
+				# Show the last normal screen.
+				pygame.display.flip()
 
 
 if __name__ == '__main__':

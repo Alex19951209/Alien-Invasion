f71e0383a141f057c9f2144f580b0a44e740fb6a
Alex19951209 <ovharuk19951209@gmail.com>
2022-09-21 18:46:38 +0200

    Ball collision detection

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,6 +84,15 @@ class AlienInvasion:
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
 
+		self._check_bullet_alien_collisions()
+
+
+	def _check_bullet_alien_collisions(self):
+		"""Respond to bullet-alien collisions."""
+		# Remove any bullets and aliens that have collided.
+		collisions = pygame.sprite.groupcollide(
+			self.bullets, self.aliens, True, True)
+
 
 	def _update_aliens(self):
 		"""

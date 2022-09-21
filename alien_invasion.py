e0e66a9330ec36d1c139b524eb83b7ba3b76d351
Alex19951209 <ovharuk19951209@gmail.com>
2022-09-21 19:55:29 +0200

    Restart the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -63,7 +63,22 @@ class AlienInvasion:
 	def _check_play_button(self, mouse_pos):
 		""""Start a new game when the player clicks Play"""
 		if self.play_button.rect.collidepoint(mouse_pos):
-			self.stats.game_active = True
+			self._start_game()
+
+
+	def _start_game(self):
+		"""Start a new game."""
+		# Reset the game statistics.
+		self.stats.reset_stats()
+		self.stats.game_active = True
+
+		# Get rid of any remaining aliens and bullets.
+		self.aliens.empty()
+		self.bullets.empty()
+
+		# Create a new fleet and center the ship.
+		self._create_fleet()
+		self.ship.center_ship()
 
 
 	def _check_keydown_events(self, event):
@@ -76,6 +91,8 @@ class AlienInvasion:
 			sys.exit()
 		elif event.key == pygame.K_SPACE:
 			self._fire_bullet()
+		elif event.key == pygame.K_p:
+			self._start_game()
 
 
 	def _check_keyup_events(self, event):
